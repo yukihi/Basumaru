3cc857ef6c0873189e5b306ac6b4469a9d4e6f67
佐久間美月 <s.nadenadeboy@gmail.com>
2015-09-02 10:09:20 +0900

    初めてのコミット

---
+++ Basumaru/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Basumaru.Startup))]
+namespace Basumaru
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
