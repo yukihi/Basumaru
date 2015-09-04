861ff835891fb02b9f8a4397e08c206c0f7239da
佐久間美月 <s.nadenadeboy@gmail.com>
2015-09-04 17:16:18 +0900

    hennkou

--- Basumaru/Controllers/HomeController.cs
+++ Basumaru/Controllers/HomeController.cs
@@ -15,21 +15,16 @@ namespace Basumaru.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
         public ActionResult DataImport()
         {
-
             return View();
         }
     }
