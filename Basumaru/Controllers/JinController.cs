2a21daa9d7e5906b259cf0d3b8dafaf586658d67
jemini777@gmail.com <jemini777@gmail.com>
2015-09-07 14:59:27 +0900

    DELETEサンプル

---
+++ Basumaru/Controllers/JinController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Basumaru.Models;
+
+namespace Basumaru.Controllers
+{
+    public class JinController : Controller
+    {
+        BasumaruDBContext db = new BasumaruDBContext();
+
+        // GET: Jin/Index
+        public ActionResult Delete()
+        {
+            ViewBag.msg = "すべてのデータを削除する場合は、「削除」ボタンをクリックしてください";
+            return View();
+        }
+
+        // POST: Jin/Index
+        [HttpPost]
+        //[ValidateAntiForgeryToken]    
+        [ValidateInput(false)]
+        public ActionResult DeleteAll()
+        {
+
+            foreach (Basutei bs in db.basutei.ToList()){
+                db.basutei.Remove(bs);
+            }
+            db.SaveChanges();
+
+            ViewBag.msg = "削除完了！";
+
+            return View("Delete");
+        }
+
+
+
+    }
+}
