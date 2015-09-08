89c7efb5911b42cc263f577a40454eb4cee4b17f
響 日塔 <sutoec@hotmail.co.jp>
2015-09-08 12:19:40 +0900

    indexの変更 homecontrollerの変更

--- Basumaru/Controllers/HomeController.cs
+++ Basumaru/Controllers/HomeController.cs
@@ -8,28 +8,64 @@ namespace Basumaru.Controllers
 {
     public class HomeController : Controller
     {
+        public string Minute { get; set; }
+        public string Name { get; set; }
+
         public ActionResult Index()
         {
+
             return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string start, string goal, string kijun, string month, string day, string hour, string minute)
+        {
+            // Sessionに各データをを格納
+            Session["start"] = start;
+            Session["goal"] = goal;
+            Session["kijun"] = kijun;
+            Session["month"] = month;
+            Session["day"] = day;
+            Session["hour"] = hour;
+            Session["minute"] = minute;
+
+
+            // 指定したページを呼び出す
+            return RedirectToAction("About");
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
+            ViewBag.MyMessage0 = (string)Session["start"];
+            ViewBag.MyMessage1 = (string)Session["goal"];
+            ViewBag.MyMessage2 = (string)Session["kijun"];
+            ViewBag.MyMessage3 = (string)Session["month"];
+            ViewBag.MyMessage4 = (string)Session["day"];
+            ViewBag.MyMessage5 = (string)Session["hour"];
+            ViewBag.MyMessage6 = (string)Session["minute"];
             return View();
+
         }
+
+
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
         public ActionResult DataImport()
         {
+            return View();
+        }
 
+        public ActionResult Busstop()
+        {
+            return View();
+        }
+
+        public ActionResult Route()
+        {
             return View();
         }
     }
