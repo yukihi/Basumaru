3c664be05738f265e12f85008fa429273be131ba
Natsuko <natsu_jaga0914@yahoo.co.jp>
2015-09-08 15:53:58 +0900

    デザインの変更

---
+++ Basumaru/Controllers/SearchExpressionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Basumaru.Controllers
+{
+    public class SearchExpressionController : Controller
+    {
+        // GET: SearchExpression
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
