412b34e2c265dbd17cac33c0f5e8178f62c79b98
響 日塔 <sutoec@hotmail.co.jp>
2015-09-09 14:25:36 +0900

    ホーム画面で使っていた絶対パスを相対パスに変更 ホーム画面のラジオボタンが渡していたデータを変更 ホームコントローラー内で現在の年の情報をSessionに格納するように変更

--- Basumaru/Controllers/HomeController.cs
+++ Basumaru/Controllers/HomeController.cs
@@ -19,10 +19,17 @@ namespace Basumaru.Controllers
         [HttpPost]
         public ActionResult Index(string start, string goal, string kijun, string month, string day, string hour, string minute)
         {
+
+            // 必要な変数を宣言する
+            DateTime dtNow = DateTime.Now;
+
+            // 年 (Year) を取得する
+            int year = dtNow.Year;
             // Sessionに各データをを格納
             Session["start"] = start;
             Session["goal"] = goal;
             Session["kijun"] = kijun;
+            Session["year"] = year;
             Session["month"] = month;
             Session["day"] = day;
             Session["hour"] = hour;
